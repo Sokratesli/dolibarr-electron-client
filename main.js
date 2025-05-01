4613346d70c22e01fc260e2e382ab42bd3484b51
Paco Krummenacher-Geiser <paco@fedora.fritz.box>
2025-05-01 09:16:11 +0200

    work in progress

--- main.js
+++ main.js
@@ -1,23 +1,48 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
+const path = require("path");
 
-function createWindow() {
-  const win = new BrowserWindow({
+let mainWindow;
+let inputWindow;
+
+function createInputWindow() {
+  inputWindow = new BrowserWindow({
+    width: 400,
+    height: 200,
+    resizable: false,
+    icon: path.join(__dirname, "icon.png"),
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      contextIsolation: true,
+    },
+  });
+
+  inputWindow.loadFile("index.html");
+}
+
+function createMainWindow(url) {
+  mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
+    icon: path.join(__dirname, "build/icons/256x256.png"),
     webPreferences: {
       contextIsolation: true,
     },
   });
 
-  win.loadURL("http://192.168.3.85:8383");
+  mainWindow.loadURL(url);
 }
 
 app.whenReady().then(() => {
-  createWindow();
+  createInputWindow();
 
   app.on("activate", () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) createInputWindow();
   });
+});
+
+ipcMain.on("open-main-window", (event, url) => {
+  createMainWindow(url);
+  inputWindow.close();
 });
 
 app.on("window-all-closed", () => {
